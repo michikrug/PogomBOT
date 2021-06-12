bfb4bca43eb0a07e7e145e3685709c2d00796105
Michael Krug <michi.krug@gmail.com>
2021-06-12 18:11:50 +0200

    Adjust json parsing

--- pogobot.py
+++ pogobot.py
@@ -1644,8 +1644,8 @@ def object_hook(obj):
     _isoformat = obj.get('_isoformat')
     if _isoformat is not None:
         return date.fromisoformat(_isoformat)
-    if type(obj) == str and obj.isnumeric():
-        return int(obj)
+    if isinstance(obj, dict):
+        return {int(k):(int(v) if type(v) == str and v.isnumeric() else v) for k,v in obj.items()}
     return obj
 
 
