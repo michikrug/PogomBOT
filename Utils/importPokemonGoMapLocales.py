f97d0e3cc9b40f7b5d42a72f54cfafd9b1151911
Michael Krug <michi.krug@gmail.com>
2023-09-10 13:23:59 +0200

    Update convert script to also include en

--- Utils/importPokemonGoMapLocales.py
+++ Utils/importPokemonGoMapLocales.py
@@ -17,14 +17,15 @@ LOGGER = logging.getLogger(__name__)
 
 def convertTo(pokemon_en, lang):
     newlang_poke = dict()
-    fileName = os.path.join(path_input, "static/locales/", lang + ".json")
-    with open(fileName, 'r', encoding='utf-8') as poke_lang_file:
-        poke_lang = json.loads(poke_lang_file.read())
+    if lang != 'en':
+        fileName = os.path.join(path_input, "static/locales/", lang + ".json")
+        with open(fileName, 'r', encoding='utf-8') as poke_lang_file:
+            poke_lang = json.loads(poke_lang_file.read())
 
     for poke_id in pokemon_en:
         try:
             en_name = pokemon_en[poke_id]['name']
-            lang_name = poke_lang[en_name]
+            lang_name = poke_lang[en_name] if lang != 'en' else en_name
             newlang_poke[poke_id] = lang_name
         except Exception as e:
             LOGGER.error("[%s] Missing: %s - %s" % (lang, poke_id, en_name))
@@ -51,7 +52,8 @@ def main():
     for file in os.listdir(path_to_locales):
         if fnmatch.fnmatch(file, '*.json'):
             convertTo(pokemon_en, file.split('.')[0])
-
+    
+    convertTo(pokemon_en, 'en')
 
 if __name__ == '__main__':
     main()
